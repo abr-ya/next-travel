53469d4e05b02f0c41eadfefdf943e14508bac73
abr-ya <gamer05@list.ru>
2023-03-09 09:14:29 +0300

    feature: Add Sanity client

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,46 @@
 import React from "react";
+import { getClient } from "../lib/sanity.server";
+import groq from "groq";
+// import Head from "next/head";
+// import Link from "next/link";
+// import Card from "../components/Card";
 import { Htag } from "@/components/index";
 import { withLayout } from "@/layout/Layout";
 
-const Home = (): JSX.Element => {
-  console.log("first");
+interface IHome {
+  posts: any;
+}
+
+const Home = ({ posts }: IHome): JSX.Element => {
+  console.log("posts", posts);
 
   return (
     <>
       <Htag tag="h1">Travel Blog Home Page)</Htag>
     </>
   );
+};
+
+export const getStaticProps = async ({ preview = false }) => {
+  console.log(getClient);
+
+  const posts = await getClient(preview).fetch(groq`
+    *[_type == "post" && publishedAt < now()] | order(publishedAt desc) {
+     _id,
+     title,
+     "username": author->username,
+     "categories": categories[]->{id, title},
+     "authorImage": author->avatar,
+     body,
+     mainImage,
+     slug,
+     publishedAt
+     }`);
+  return {
+    props: {
+      posts,
+    },
+  };
 };
 
 export default withLayout(Home);
--- src/layout/Sidebar/Sidebar.tsx
+++ src/layout/Sidebar/Sidebar.tsx
@@ -4,15 +4,15 @@ import styles from "./Sidebar.module.css";
 import cn from "classnames";
 import logo from "../logo.png";
 import { Htag } from "@/components/index";
-import Link from "next/link";
+// import Link from "next/link";
 
 export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
   return (
     <div className={cn(className, styles.sidebar)} {...props}>
       <Htag tag="h2">Sidebar</Htag>
-      <Link href="/">
-        <Image src={logo} alt="Travel Blog Logo" width={80} height={80} />
-      </Link>
+      {/* <Link href="/"> */}
+      <Image src={logo} alt="Travel Blog Logo" width={80} height={80} />
+      {/* </Link> */}
     </div>
   );
 };
